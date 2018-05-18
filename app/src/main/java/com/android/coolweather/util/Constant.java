5de22eb87b497b5b78504e1db72dfe685cfc0098
Joesa <1206448302@qq.com>
2018-05-18 16:15:37 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/android/coolweather/util/Constant.java
@@ -0,0 +1,15 @@
+package com.android.coolweather.util;
+
+/**
+ * Created by peizhuo on 2018/5/18.
+ */
+
+public class Constant {
+
+    public static final String TYPE_PROVINCE = "province";
+    public static final String TYPE_CITY = "city";
+    public static final String TYPE_COUNTY = "county";
+
+    public static final String LOADING = "正在加载...";
+    public static final String LOAD_FAILURE = "加载失败";
+}
